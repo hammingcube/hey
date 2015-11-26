d9c6624e23a5af139d3edfbff0a9ed37c2a2d980
Madhav Jha <madhavjha@gmail.com>
2015-11-26 09:32:43 -0800

    More

--- cmd/build.go
+++ cmd/build.go
@@ -112,6 +112,8 @@ to quickly create a Cobra application.`,
 
 func init() {
 	RootCmd.AddCommand(buildCmd)
+	//out := new(bytes.Buffer)
+	//fmt.Println(string(out.Bytes()))
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
